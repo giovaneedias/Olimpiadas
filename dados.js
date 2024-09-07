5ffdfc278605c93287ebc157eb1e299950770e04
giovaneedias <giovane_07@icloud.com>
2024-09-07 20:22:18 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        título: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu estilo único e talento precoce. Com apenas idade, ela já coleciona títulos importantes e inspira milhões de jovens a seguirem seus sonhos.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+
+    },
+
+    {
+        título: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que vem se destacando no cenário internacional. Com sua técnica apurada e determinação, ela conquistou diversas medalhas e representa o futuro do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags : "judô judo judoka"
+    },
+
+    {
+        título: "Rebeca Andrade",
+        descricao: "Rebeca Andrade, a joia da ginástica brasileira, conquistou o mundo com seu talento e determinação. Com uma série de medalhas olímpicas e mundiais, ela se tornou um dos maiores nomes do esporte, inspirando gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "ginastica ginástica ouro"
+    }
+]
+
